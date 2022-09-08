2876989772183ca139c2a4bcbd70f77e447481c6
Otto <otja@itu.dk>
2022-09-09 00:42:54 +0200

    Created user interface for isLeapYear

--- console/LeapYear.cs
+++ console/LeapYear.cs
@@ -1,6 +1,16 @@
+using System;
+
 namespace console;
 
 public class LeapYear {
+    public static void Main(String[] args){
+
+        var leap = new LeapYear();
+
+        int year = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(leap.printLeapYear(year));
+    }
     public bool isLeapYear(int year) {
         if (year % 4 == 0)
         {
@@ -15,5 +25,19 @@ public class LeapYear {
             return true;
         }
         return false;
+    }
+
+    public string printLeapYear(int year)
+    {
+
+        var leap = new LeapYear();
+
+        if (leap.isLeapYear(year))
+        {
+            return "yay";
+        } else
+        {
+            return "nay";
+        }
     }
 }
